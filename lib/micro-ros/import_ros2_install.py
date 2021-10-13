b3401dd0980f60e4b33c37fd5eace0c649a673e2
Tiger3018 <tiger3018of02@gmail.com>
2021-10-13 19:46:13 +0800

    test

---
+++ lib/micro-ros/import_ros2_install.py
@@ -0,0 +1,21 @@
+Import("env")
+global_env = DefaultEnvironment()
+
+import os 
+
+env.Append(LINKFLAGS=['-Wl,--start-group'])
+for package in os.listdir():
+    if os.path.isdir(package):
+        if os.path.exists(package + "/include"):
+            env.Append(CPPPATH=[os.path.abspath(package + "/include")]) 
+        # if os.path.exists(package + "/lib"):
+        #     for staticLib in os.listdir(package + '/lib'):
+        #         if os.path.isfile(package + '/lib/' + staticLib):
+        #             env.Append(LINKFLAGS=[os.path.abspath(package + "/lib/" + staticLib)])
+env.Append(LINKFLAGS=[os.path.abspath('libmicroros.a')])
+            # print(package)
+# env.Append(LINKFLAGS=['-Wl,lib/micro-ros/rclc/lib/librclc.a'])
+# env.Append(CPPEFINES=['-DUNREAL'])
+# env.Append(CCFLAGS=['-DUNREAL'])
+
+# print(env.Dump())
